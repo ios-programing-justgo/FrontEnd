476a6863be608e3bc771815ef8a35dc23ab9f9a8
Hansa Chen <hansa.chen@gmail.com>
2019-05-07 17:55:14 -0400

    updated store

--- JustGo/JustGo/Store.swift
+++ JustGo/JustGo/Store.swift
@@ -12,16 +12,17 @@ import UIKit
 class Store: NSObject {
     var name:String = "No Name"
     var storeID = "No Id"
+    var count = "0"
 //    var drinks = [Item]()
 //    var food = [Item]()
     var address:String = "No addr."
     var lat:String = ""
     var lon:String = ""
     
-    init(name:String, storeID:String,address:String,lat:String,lon:String){
+    init(name:String,storeID:String,address:String,lat:String,lon:String,count:String){
         self.name = name
         self.storeID = storeID
-        
+        self.count = count
         self.address = address
         self.lat = lat
         self.lon = lon
